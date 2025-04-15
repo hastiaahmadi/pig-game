61c6d0730025ff64c8f11cd95f58963ea055bad5
Hastia <ahmadi.dev05@gmail.com>
2025-04-15 11:53:00 +0330

    Update script.js

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ btnRoll.addEventListener('click', function(){
 
     //2.Display dice
     diceEl.classList.remove('hidden');
-    diceEl.src=`public/images/dice-${dice}.png`;
+    diceEl.src=`public/images/dice${dice}.png`;
 
     //3.Check for rolled 1;
     if(dice!==1){
